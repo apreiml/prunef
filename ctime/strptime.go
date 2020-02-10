b90a6168a465daa74bf42d79a2bd22c8660c59e8
Armin Preiml <apreiml@strohwolke.at>
2020-02-10 19:50:53 +0100

    parse %Y

--- ctime/strptime.go
+++ ctime/strptime.go
@@ -3,29 +3,87 @@ package ctime
 import (
     "time"
     "fmt"
+    "strconv"
+    "errors"
+    "log"
 )
 
 type parseState struct {
     format []rune
-    s []rune
-    t *time.Time
+    value []rune
+    indexFormat, indexS int
+    year, month, day, hour, min, sec int
 }
 
 
-func Strptime(s, format string, t *time.Time) error {
-    state := parseState{[]rune(format), []rune(s), t}
+func Parse(format, value string, loc *time.Location) (t time.Time, err error) {
+    state := parseState{format: []rune(format), value: []rune(value), year: 1, month: 1, day: 1}
 
     defer func () {
         if r := recover(); r != nil {
             fmt.Println("Recoverd parsing")
+            err = errors.New(fmt.Sprintf("Parsing failed: %s", r))
         }
     }()
 
-    return state.parse()
+    state.parse()
+
+    return time.Date(state.year, time.Month(state.month), state.day, state.hour,
+                     state.min, state.sec, 0, loc), nil
 }
 
-func (state *parseState) parse() error {
-    fmt.Println("parsing")
-    return nil
+func (state *parseState) isEndOfFormat() bool {
+    return state.indexFormat >= len(state.format)
+}
+
+func (state *parseState) readFormat() rune {
+    f := state.format[state.indexFormat]
+    state.indexFormat += 1
+    return f
+}
+
+func (state *parseState) read(chars int) []rune {
+    begin := state.indexS
+    end := begin + chars
+
+    state.indexS = end
+    return state.value[begin:end]
+}
+
+func (state *parseState) readInt(chars int) int {
+    log.Printf("read int of length %d", chars)
+    i, err := strconv.Atoi(string(state.read(chars)))
+    if err != nil {
+        panic("Parse int error")
+    }
+    log.Printf("int read %d", i)
+    return i
+}
+
+func (state *parseState) parse() {
+    for {
+        if state.isEndOfFormat() {
+            break
+        }
+        f := state.readFormat()
+
+        if f == '%' {
+            state.parseFormat()
+        } else {
+            c := state.read(1)[0]
+            log.Printf("Got format %c compare to %c", f, c)
+            if c != f {
+                panic(fmt.Sprintf("Unexpected char %c, expected: %c", c, f))
+            }
+        }
+    }
+}
+
+func (state *parseState) parseFormat() {
+    switch f := state.readFormat(); f {
+    case 'Y':
+        log.Printf("parse year")
+        state.year = state.readInt(4)
+    }
 }
 
--- ctime/strptime_test.go
+++ ctime/strptime_test.go
@@ -4,22 +4,39 @@ import (
     "testing"
     "time"
 )
-
-func TestSimple(t *testing.T) {
-    now := time.Now()
-
+func TestParseFail(t *testing.T) {
     cases := []struct {
         format, in string
-        t *time.Time
     }{
-        {"nodate", "nodate", &now},
+        {"asdf", "jkld"},
     }
 
     for _, c := range cases {
-        tt := time.Time(now)
-        Strptime(c.format, c.in, &tt)
-        if *c.t != tt {
-            t.Errorf("Strptime(%s, %s) == %s want %s", c.format, c.in, tt, c.t)
+        _, err := Parse(c.format, c.in, time.UTC)
+        if err == nil {
+            t.Errorf("Strptime(%s, %s) should fail", c.format, c.in)
+        }
+    }
+}
+
+func TestSimple(t *testing.T) {
+    cases := []struct {
+        format, in string
+        expected string
+    }{
+        {"nodate", "nodate", "0001-01-01T00:00:00Z"},
+        {"%Y", "2020", "2020-01-01T00:00:00Z"},
+    }
+
+    for _, c := range cases {
+        result, err := Parse(c.format, c.in, time.UTC)
+        if err != nil {
+            t.Errorf("failed Parsing: %s", err)
+        }
+
+        expectedTime, _ := time.Parse(time.RFC3339, c.expected)
+        if expectedTime != result {
+            t.Errorf("Strptime(%s, %s) == %s want %s", c.format, c.in, result, c.expected)
         }
     }
 }
