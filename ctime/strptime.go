0bbe3f0407561bd152fe52d4f4c730dcec0808f5
Armin Preiml <apreiml@strohwolke.at>
2020-02-11 18:44:29 +0100

    refactor ctime structs

--- ctime/strptime.go
+++ ctime/strptime.go
@@ -1,115 +1,118 @@
 package ctime
 
 import (
-    "time"
-    "fmt"
-    "strconv"
-    "errors"
-    "log"
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
 )
 
-type parseState struct {
-    format []rune
-    value []rune
-    indexFormat, indexS int
-    year, month, day, hour, minute, second int
+type tm struct {
+	year, month, day, hour, minute, second int
+	loc                                    *time.Location
 }
 
+type parseState struct {
+	format              []rune
+	value               []rune
+	indexFormat, indexS int
+	time                tm
+}
 
 func Parse(format, value string, loc *time.Location) (t time.Time, err error) {
-    state := parseState{format: []rune(format), value: []rune(value), year: 1, month: 1, day: 1}
+	tm := tm{year: 1, month: 1, day: 1, loc: loc}
+	state := parseState{format: []rune(format), value: []rune(value), time: tm}
 
-    defer func () {
-        if r := recover(); r != nil {
-            fmt.Println("Recoverd parsing")
-            err = errors.New(fmt.Sprintf("Parsing failed: %s", r))
-        }
-    }()
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recoverd parsing")
+			err = errors.New(fmt.Sprintf("Parsing failed: %s", r))
+		}
+	}()
 
-    state.parse()
+	state.parse()
+	return state.time.toTime(), nil
+}
 
-    return time.Date(state.year, time.Month(state.month), state.day, state.hour,
-                     state.minute, state.second, 0, loc), nil
+func (t *tm) toTime() time.Time {
+	return time.Date(t.year, time.Month(t.month), t.day, t.hour,
+		t.minute, t.second, 0, t.loc)
 }
 
 func (state *parseState) isEndOfFormat() bool {
-    return state.indexFormat >= len(state.format)
+	return state.indexFormat >= len(state.format)
 }
 
 func (state *parseState) readFormat() rune {
-    f := state.format[state.indexFormat]
-    state.indexFormat += 1
-    return f
+	f := state.format[state.indexFormat]
+	state.indexFormat += 1
+	return f
 }
 
 func (state *parseState) read(chars int) []rune {
-    begin := state.indexS
-    end := begin + chars
+	begin := state.indexS
+	end := begin + chars
 
-    state.indexS = end
-    return state.value[begin:end]
+	state.indexS = end
+	return state.value[begin:end]
 }
 
 func (state *parseState) expect(expected string) {
-    got := string(state.read(len(expected)))
-    if got != expected {
-        panic(fmt.Sprintf("Didn't expect %s", got))
-    }
+	got := string(state.read(len(expected)))
+	if got != expected {
+		panic(fmt.Sprintf("Didn't expect %s", got))
+	}
 }
 
 func (state *parseState) readInt(chars int) int {
-    log.Printf("read int of length %d", chars)
-    i, err := strconv.Atoi(string(state.read(chars)))
-    if err != nil {
-        panic("Parse int error")
-    }
-    log.Printf("int read %d", i)
-    return i
+	i, err := strconv.Atoi(string(state.read(chars)))
+	if err != nil {
+		panic("Parse int error")
+	}
+	return i
 }
 
 func (state *parseState) parse() {
-    for {
-        if state.isEndOfFormat() {
-            break
-        }
-        f := state.readFormat()
+	for {
+		if state.isEndOfFormat() {
+			break
+		}
+		f := state.readFormat()
 
-        if f == '%' {
-            state.parseFormat()
-        } else {
-            c := state.read(1)[0]
-            log.Printf("Got format %c compare to %c", f, c)
-            if c != f {
-                panic(fmt.Sprintf("Unexpected char %c, expected: %c", c, f))
-            }
-        }
-    }
+		if f == '%' {
+			state.parseFormat()
+		} else {
+			c := state.read(1)[0]
+			if c != f {
+				panic(fmt.Sprintf("Unexpected char %c, expected: %c", c, f))
+			}
+		}
+	}
 }
 
 func (state *parseState) parseFormat() {
-    switch f := state.readFormat(); f {
-    case '%':
-        state.expect("%")
-    case 'Y':
-        state.year = state.readInt(4)
-    case 'm':
-        state.month = state.readInt(2)
-    case 'd':
-        state.day = state.readInt(2)
-    case 'D':
-        state.month = state.readInt(2)
-        state.expect("/")
-        state.day = state.readInt(2)
-        state.expect("/")
-        state.year = state.readInt(4)
-    case 'H':
-        state.hour = state.readInt(2)
-    case 'M':
-        state.minute = state.readInt(2)
-    case 'S':
-        state.second = state.readInt(2)
-    default:
-        panic(fmt.Sprintf("Unsupported format specifier %c. Patches are welcome.", f))
-    }
+	switch f := state.readFormat(); f {
+	case '%':
+		state.expect("%")
+	case 'Y':
+		state.time.year = state.readInt(4)
+	case 'm':
+		state.time.month = state.readInt(2)
+	case 'd':
+		state.time.day = state.readInt(2)
+	case 'D':
+		state.time.month = state.readInt(2)
+		state.expect("/")
+		state.time.day = state.readInt(2)
+		state.expect("/")
+		state.time.year = state.readInt(4)
+	case 'H':
+		state.time.hour = state.readInt(2)
+	case 'M':
+		state.time.minute = state.readInt(2)
+	case 'S':
+		state.time.second = state.readInt(2)
+	default:
+		panic(fmt.Sprintf("Unsupported format specifier %c. Patches are welcome.", f))
+	}
 }
-
--- main.go
+++ main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "time"
-    "git.sr.ht/apreiml/prunef/ctime"
+	"flag"
+	"fmt"
+	"git.sr.ht/apreiml/prunef/ctime"
+	"os"
+	"time"
 )
 
 func main() {
-    var fl = flag.Int("flagname", 1234, "help message for flagname")
-    flag.Parse()
-    fmt.Printf("%d\n", *fl)
+	var fl = flag.Int("flagname", 1234, "help message for flagname")
+	flag.Parse()
+	fmt.Printf("%d\n", *fl)
 
-    if len(flag.Args()) != 1 {
-        printUsage()
-        os.Exit(1)
-    }
+	if len(flag.Args()) != 1 {
+		printUsage()
+		os.Exit(1)
+	}
 
-    var formatString = flag.Args()[0]
+	var formatString = flag.Args()[0]
 
-    var val, err = time.Parse("thor.server.aktrophy.at-2019-11-02_19-00-03", formatString)
-    fmt.Println(err)
-    fmt.Println(val)
+	var val, err = time.Parse("thor.server.aktrophy.at-2019-11-02_19-00-03", formatString)
+	fmt.Println(err)
+	fmt.Println(val)
 
-    var t = time.Now()
-    fmt.Printf("%#v\n", t)
-    ctime.Strptime("asdf", "asdf", &t)
+	var t = time.Now()
+	fmt.Printf("%#v\n", t)
+	ctime.Strptime("asdf", "asdf", &t)
 }
 
 func printUsage() {
-    fmt.Printf("Wrong args\n")
+	fmt.Printf("Wrong args\n")
 }
-
